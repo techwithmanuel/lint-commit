94baca3066ad3fcb050f099cb21ba8dcc3c6cd36
Emmanuel Alozie <emmanuelalozie@Emmanuels-MacBook-Pro.local>
2024-05-19 17:44:15 +0100

    fix: update prompt with lowercase requirement}

--- generate-commit.js
+++ generate-commit.js
@@ -1,21 +1,24 @@
+#!/usr/bin/env node
+
 import { config } from "dotenv";
+import chalk from "chalk";
 config();
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function generateCommit(input) {
-  // For text-only input, use the gemini-pro model
   try {
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const prompt = `Using the commit lint convention, write a commit message in a single sentence for : ${input}`;
+    const prompt = `Using the commit lint convention, in all lowercase characters, write a commit message in a single sentence for : ${input}`;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     return text;
   } catch (error) {
-    throw new Error(error);
+    console.log(chalk.red(`${error}`));
+    process.exit(1);
   }
 }
